dc0a16d2e2b33559988bc794420b0a10d8b61500
Jannik <8103530+wh1zk1d@users.noreply.github.com>
2023-10-16 09:01:08 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+<p align="center"><img src="https://emojicdn.elk.sh/🩻" width="80" alt="X-Ray Emoji"></p>
+
+# wilhelm
+
+wilhelm is a tiny app that works like [Ray](https://github.com/spatie/ray), but with only a third of the features. I built it because I love Ray and wanted to learn how it works. It's built with Node.js and WebSockets.
+
+If you need a serious debugging tool, please use [Ray](https://github.com/spatie/ray) – it's awesome!
---
+++ src/index.ts
@@ -0,0 +1,37 @@
+import express from "express";
+import WebSocket from "ws";
+import http from "http";
+import path from "path";
+
+const app = express();
+const port = 23517;
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+app.use(express.json());
+
+wss.on("connection", (ws) => {
+  console.log("establish websocket connection");
+
+  ws.on("message", (message) => {
+    console.log("received: %s", message);
+  });
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, `../public/index.html`));
+});
+
+app.post("/ray", (req, res) => {
+  wss.clients.forEach((client) => {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(req.body.data);
+    }
+  });
+
+  res.sendStatus(200);
+});
+
+server.listen(port, () =>
+  console.log(`http server is listening on http://localhost:${port}`)
+);
